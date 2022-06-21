44cf2074ffeb53c19145e62b6904a224734ee369
Valérie Turgeon <valerie.turgeon.cem@gmail.com>
2022-06-20 21:43:24 -0400

    INIT Création du projet initial

---
+++ Appl_TestsUnitaires/Appl_TestsUnitaires/E2_EvenNumbers.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Appl_TestsUnitaires
+{
+  public class E2_EvenNumbers
+  {
+    public IEnumerable<int> GetEvenNumbers(int MaxValue)
+    {
+      for (var i = 0; i <= MaxValue; i++)
+        if (i % 2 == 0)
+          yield return i;
+    }
+  }
+}
---
+++ Appl_TestsUnitaires/Appl_TestsUnitaires/E3_AttackPointsCalculator.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Appl_TestsUnitaires
+{
+  public class E3_AttackPointsCalculator
+  {
+    private const int NbrAttackMin = 20;
+    private const int NbrAttackMax = 200;
+
+    public int CalculateAttackPoints(int nbrAttacks)
+    {
+      if (nbrAttacks < 0 || nbrAttacks > NbrAttackMax)
+        throw new ArgumentOutOfRangeException();
+
+      if (nbrAttacks <= NbrAttackMin) return 0;
+
+      const int AttackPerPoint = 5;
+      var AttackPoints = (nbrAttacks - NbrAttackMin) / AttackPerPoint;
+
+      return AttackPoints;
+    }
+  }
+}
