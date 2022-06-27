09a9c76147057761291f0e39e6981e3e6d02ce32
Valérie Turgeon <valerie.turgeon.cem@gmail.com>
2022-06-27 11:10:48 -0400

    FCT Création Commutateur de langue i18n
    Création de la PartialView pour la sélection de la langue et l’enregistrement du Cookie dans le HomeController

--- ZombieParty/ZombieParty/Controllers/HomeController.cs
+++ ZombieParty/ZombieParty/Controllers/HomeController.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Localization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Localization;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
@@ -12,10 +15,14 @@ namespace ZombieParty.Controllers
   public class HomeController : Controller
   {
     private readonly ILogger<HomeController> _logger;
+        //TODO 07: Injection des resources localizer locals / shared
+        private readonly IStringLocalizer<HomeController> _localizer;
 
-    public HomeController(ILogger<HomeController> logger)
+
+        public HomeController(ILogger<HomeController> logger, IStringLocalizer<HomeController> localizer)
     {
       _logger = logger;
+      _localizer = localizer;
     }
 
     public IActionResult Index()
@@ -28,7 +35,23 @@ namespace ZombieParty.Controllers
       return View();
     }
 
-    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    [HttpPost]
+    public IActionResult SetLanguage(string culture, string returnUrl)
+    {
+        var cookie = CookieRequestCultureProvider.MakeCookieValue(new RequestCulture(culture));
+        var name = CookieRequestCultureProvider.DefaultCookieName;
+
+        Response.Cookies.Append(name, cookie, new CookieOptions
+        {
+            Path = "/",
+            Expires = DateTimeOffset.UtcNow.AddYears(1),
+        });
+
+        return LocalRedirect(returnUrl);
+    }
+
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public IActionResult Error()
     {
       return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
