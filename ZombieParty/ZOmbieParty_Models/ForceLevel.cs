69a96b0fc3d19d86d90ba18b4c38a0aadfcb621f
Valérie Turgeon <valerie.turgeon.cem@gmail.com>
2022-06-27 11:13:40 -0400

    FCT ForceLevel

---
+++ ZombieParty/ZOmbieParty_Models/ForceLevel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ZombieParty_Models
+{
+  public class ForceLevel
+  {
+    [Key]
+    public int Id { get; set; }
+    [Range(5, 10)]
+    public int ForceLevelNiv { get; set; }
+
+    // Propriété de navigation
+    //OBLIGATOIRE Pour la relation 1 à plusieurs avec Zombies
+    public ICollection<Zombie> Zombies { get; set; }
+  }
+}
--- ZombieParty/ZOmbieParty_Models/Zombie.cs
+++ ZombieParty/ZOmbieParty_Models/Zombie.cs
@@ -31,9 +31,17 @@ namespace ZombieParty_Models
     //OBLIGATOIRE Pour la relation 1 à plusieurs avec ZombieType
     public virtual ZombieType ZombieType { get; set; }
 
-    // Propriété de navigation vers zombieHuntingLog
-    //OBLIGATOIRE Pour la relation 1 à plusieurs avec zombieHuntingLog
-    public ICollection<ZombieHuntingLog> zombieHuntingLogs { get; set; }
+        // FACULTATIF on peut formellement identifier le champ lien
+        [ForeignKey("ForceLevel")]
+        // Un Zombie PEUT ou non avoir un ForceLevel, donc int peut être null
+        public int? ForceLevelId { get; set; }
+        //Propriété de navigation
+        //OBLIGATOIRE Pour la relation 1 à plusieurs avec ForceLevel
+        public virtual ForceLevel ForceLevel { get; set; }
+
+        // Propriété de navigation vers zombieHuntingLog
+        //OBLIGATOIRE Pour la relation 1 à plusieurs avec zombieHuntingLog
+        public ICollection<ZombieHuntingLog> zombieHuntingLogs { get; set; }
 
     }
 
